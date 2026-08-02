63a8d8faa4d0654f7ad1289f900387f182e56d38
yoyert29 <yoyert2020@gmail.com>
2026-08-02 16:48:19 -0500

    Reorganizing the project. Seperated the physics functions. Included a function to calculate net force. Included files physics.py, rocket.py. integrator.py, atmosphere.py. Currently working on main.py and integrator.py.

---
+++ integrator.py
@@ -0,0 +1,3 @@
+import math
+
+#This document will perform the numeric integration: Euler, RK4, etc. 
---
+++ physics.py
@@ -0,0 +1,23 @@
+import math
+
+GRAVITY = 9.80665
+
+def gravity_force(state):
+    return (0, -GRAVITY * state.mass)
+
+def drag_force(rocket, state, fluid):
+    speed = math.hypot(state.velocity_x, state.velocity_y)
+    if speed == 0:
+        return (0.0, 0.0)
+
+    drag_force = 0.5 * rocket.front_area * rocket.drag_coefficient * speed * speed * fluid.density
+    drag_force_x = drag_force * -state.velocity_x / speed
+    drag_force_y = drag_force * -state.velocity_y / speed
+
+    return (drag_force_x, drag_force_y)
+
+def net_force(gravity, drag):
+    x = gravity[0] + drag[0]
+    y = gravity[0] + drag[0]
+    return (x, y)
+
